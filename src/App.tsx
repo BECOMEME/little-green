690bc2db0b88426f0696daac158e91a8c0c5a318
stoneuo <y.li25@student.vu.nl>
2024-11-09 13:56:17 +0100

    change name form

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
 import "./globals.css";
-import RegisterForm from './_auth/Form/registerForm';
-import { Home } from './_root/Pages';
-import AuthLayoutForm from './_auth/authLayoutForm';
-import LoginForm from './_auth/Form/LoginForm';
-import RootLayout from './_root/RootLayout';
-
+import RegisterForm from "./_auth/forms/registerForm";
+import { Home } from "./_root/Pages";
+import AuthLayoutForm from "./_auth/authLayoutForm";
+import LoginForm from "./_auth/forms/LoginForm";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
-    return (
-        <main className="flex full_screen">
-            <Routes>
-                <Route element={<AuthLayoutForm />}>
-                    <Route path="/log-in" element={<LoginForm />} />
-                    <Route path="/register" element={<RegisterForm />} />
-                </Route>
-                <Route element={<RootLayout />}>
-                    <Route index element={<Home />} />
-                </Route>
-            </Routes>
+  return (
+    <main className="flex full_screen">
+      <Routes>
+        <Route element={<AuthLayoutForm />}>
+          <Route path="/log-in" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterForm />} />
+        </Route>
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
+  );
+};
 
-        </main>
-    )
-}
-
-export default App
+export default App;
