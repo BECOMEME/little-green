4f22922eafbbc78561f5617014882ec72ac910e7
stoneuo <y.li25@student.vu.nl>
2024-11-09 15:13:26 +0100

    form

--- src/_auth/authLayoutForm.tsx
+++ src/_auth/authLayoutForm.tsx
@@ -9,8 +9,12 @@ const AuthLayoutForm = () => {
         <Navigate to="/" />
       ) : (
         <>
-          {" "}
-          <section>
+          <img
+            src="public/assests/fourleaves.jpg"
+            alt="logo"
+            className="hidden xl:block h-screen w-1/2 object-cover bg-no-repeat"
+          />
+          <section className="flex flex-1 justify-center items-center flex-col py-10">
             <Outlet />
           </section>
         </>
